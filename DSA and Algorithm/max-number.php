3a95b6dfe5783988d0dc5d31c8d9fb43c4899ef7
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-04-16 11:41:33 +0600

    Update max-number.php

--- DSA and Algorithm/max-number.php
+++ DSA and Algorithm/max-number.php
@@ -1,17 +1,13 @@
 <?php
 // PHP script to find the largest number in an array
 
-$arr = [15, 2, 4, 7, 9, 25];
-$temp = 0;
-
-// Loop through the array
-for ($i = 0; $i < count($arr); $i++) {
-    // If current element is bigger than temp, update temp
-    if ($arr[$i] > $temp) {
-        $temp = $arr[$i];
-    }
+function FindLeargeNumber( $arr ){
+  $LeargeNumber = $arr[0];
+  for( $i = 0; $i<count($arr); $i++ ){
+      if( $arr[$i] > $LgNumber ){
+          $LgNumber = $arr[$i];
+      }
+  }
+  echo $LeargeNumber;
 }
-
-// Print the largest number after the loop
-echo "The largest number is: " . $temp;
-?>
+FindLeargeNumber([2,35,6,7,3]);
