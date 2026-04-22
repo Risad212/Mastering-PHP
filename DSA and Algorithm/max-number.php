19e99f2be2dc6fc60b993e7388d7a98ffd86a9c0
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-04-22 10:09:29 +0600

    Refactor function to find largest number in array

--- DSA and Algorithm/max-number.php
+++ DSA and Algorithm/max-number.php
@@ -1,13 +1,24 @@
 <?php
-// PHP script to find the largest number in an array
 
-function FindLeargeNumber( $arr ){
-  $LeargeNumber = $arr[0];
+function findLargestNumber( $arr ){
+     if (empty($arr)) {
+        echo "Array is empty";
+        return;
+    }
+    
+  $largestNumber = $arr[0];
   for( $i = 0; $i<count($arr); $i++ ){
-      if( $arr[$i] > $LgNumber ){
-          $LgNumber = $arr[$i];
+      if( $arr[$i] > $largestNumber ){
+          $largestNumber = $arr[$i];
       }
   }
-  echo $LeargeNumber;
+  echo $largestNumber. ' ';
 }
-FindLeargeNumber([2,35,6,7,3]);
+
+findLargestNumber([-10, -50, -1, -30, -40]); // -1 ✅
+findLargestNumber([1, 2, 3, 4, 5]);          // 5 ✅
+findLargestNumber([5, 5, 5, 5]);             // 5 ✅
+findLargestNumber([100]);                   // 100 ✅
+findLargestNumber([-1]);                    // -1 ✅
+findLargestNumber([]);                      // Array is empty ✅
+findLargestNumber([0, -1, -2, -3]);         // 0 ✅
