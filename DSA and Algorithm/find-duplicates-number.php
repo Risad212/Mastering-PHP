b8b0de81ad3ba1b3d85d534bbceee14f5cb3a3fd
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-04-22 16:03:40 +0600

    Implement findDuplicateNumber function
    Added a function to find duplicate numbers in an array with test cases.

--- DSA and Algorithm/find-duplicates-number.php
+++ DSA and Algorithm/find-duplicates-number.php
@@ -1,5 +1,6 @@
 <?php
 
+// broute fource approce
 $arr = [1, 2, 3, 3, 4, 2, 1];
 $duplicates = []; 
 
@@ -12,3 +13,47 @@ for ($i = 0; $i < count($arr); $i++) {
 }
 
 print_r($duplicates);
+
+// On Time complexity 
+function findDuplicateNumber($arr) {
+
+    $seen = [];
+    $duplicate = [];
+
+    for ($i = 0; $i < count($arr); $i++) {
+
+        $val = $arr[$i];
+
+        // if already seen before
+        if (isset($seen[$val])) {
+
+            // only add once to duplicate list
+            if (!in_array($val, $duplicate)) {
+                $duplicate[] = $val;
+            }
+
+        } else {
+            $seen[$val] = true;
+        }
+    }
+
+    return $duplicate;
+}
+
+// 🧪 TEST CASES
+
+print_r(findDuplicateNumber([])); // []
+
+print_r(findDuplicateNumber([1,4,5,3,73,2,3,3])); // [3]
+
+print_r(findDuplicateNumber([-1,-2,-1, -10, -3])); // [-1]
+
+print_r(findDuplicateNumber([null, null])); // []
+
+print_r(findDuplicateNumber([2,2,2,2])); // [2]
+
+print_r(findDuplicateNumber([1,2,3,4,5])); // []
+
+print_r(findDuplicateNumber([0, "0", 0])); // [0]
+
+print_r(findDuplicateNumber([true, 1, true])); // [1]
