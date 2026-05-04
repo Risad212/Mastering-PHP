96a3643d595d2c3cbdecdb4ff642fea165c8eb66
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-05-04 14:29:19 +0600

    Update most-frequent-number.php

--- DSA and Algorithm/most-frequent-number.php
+++ DSA and Algorithm/most-frequent-number.php
@@ -21,3 +21,26 @@ function mostFrequent(array $arr) {
   return $result;
 }
 print_r( mostFrequent( [5, 1, 5, 2, 5, 3] ) );
+
+
+//================= one loop ===================
+
+function mostFrequent(array $arr) {
+  $seen = [];
+  $maxCount = 0;
+  $result = null;
+ foreach( $arr as $num ){
+    if( isset( $seen[$num] ) ){
+        $seen[$num]++;
+    }else{
+      $seen[$num] = 1;
+    }
+    if( $seen[$num] > $maxCount ){
+        $maxCount = $seen[$num];
+        $result = "$num is learget $seen[$num] time";
+    }
+  }
+  print_r($seen);
+  return $result;
+}
+print_r( mostFrequent( [5, 1, 5, 2, 5, 3] ) );
