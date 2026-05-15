e9fea912469b1cef9061785b44709029356973e6
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-05-15 10:20:07 +0600

    Refactor two-sum function to use for loop

--- DSA and Algorithm/two-sum-in-shortarray.php
+++ DSA and Algorithm/two-sum-in-shortarray.php
@@ -1,12 +1,10 @@
 <?php
-// Online PHP compiler to run PHP program online
-// Print "Start small. Ship something." message
 
 function findTarget($arr, $target) {
     $start = 0;
     $end = count($arr) - 1;
 
-    while ($start < $end) {
+    for ($i = 0; $i < count($arr); $i++) {
         $sum = $arr[$start] + $arr[$end];
 
         if ($sum === $target) {
@@ -24,4 +22,3 @@ function findTarget($arr, $target) {
 $arr = [1,2,3,4,5,6,7];
 
 print_r(findTarget($arr, 9));
-
